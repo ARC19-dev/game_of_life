cb48d79a1a5b3e3f2f6e398996f868a50f61a8e0
aref hormozi <65944186+ARC19-dev@users.noreply.github.com>
2020-10-21 23:35:53 +0330

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib
 import argparse
 import time
 
+# matplotlib.use('Qt5Agg')
+
 class Board(object):
-   def __init__(self, size, seed = 'Random'):
+   def __init__(self, *size, seed='Random'):
       if seed == 'Random':
-         self.state = np.random.randint(2, size = size)
+         self.state = np.random.randint(2, size=size)
       self.engine = Engine(self)
       self.iteration = 0
+
    def animate(self):
       i = self.iteration
       im = None
@@ -17,7 +20,7 @@ class Board(object):
       while True:
          if i == 0:
             plt.ion()
-            im = plt.imshow(self.state, vmin = 0, vmax = 2, cmap = plt.cm.gray)
+            im = plt.imshow(self.state, vmin=0, vmax=2, cmap=plt.cm.gray)
          else:
             im.set_data(self.state)
          i += 1
@@ -29,30 +32,31 @@ class Board(object):
 class Engine(object):
    def __init__(self, board):
       self.state = board.state
+
    def countNeighbors(self):
-      state = self.state
-      n = (state[0:-2,0:-2] + state[0:-2,1:-1] + state[0:-2,2:] +
-          state[1:-1,0:-2] + state[1:-1,2:] + state[2:,0:-2] +
-          state[2:,1:-1] + state[2:,2:])
+      
+      n = (self.state[0:-2,0:-2] + self.state[0:-2,1:-1] + self.state[0:-2,2:] +
+          self.state[1:-1,0:-2] + self.state[1:-1,2:] + self.state[2:,0:-2] +
+          self.state[2:,1:-1] + self.state[2:,2:])
       return n
+   
    def applyRules(self):
       n = self.countNeighbors()
       state = self.state
       birth = (n == 3) & (state[1:-1,1:-1] == 0)
       survive = ((n == 2) | (n == 3)) & (state[1:-1,1:-1] == 1)
       state[...] = 0
-      state[1:-1,1:-1][birth | survive] = 1
-      nBirth = np.sum(birth)
-      self.nBirth = nBirth
-      nSurvive = np.sum(survive)
-      self.nSurvive = nSurvive
+      state[1:-1,1:-1][birth | survive] = 1 
+      self.nBirth = np.sum(birth)
+      self.nSurvive = np.sum(survive)
       return state
 
-ap = argparse.ArgumentParser(add_help = False) # Intilialize Argument Parser
-ap.add_argument('-h', '--height', help = 'Board Height', default = 256)
-ap.add_argument('-w', '--width', help = 'Board Width', default = 256)
-args = vars(ap.parse_args()) # Gather Arguments
-bHeight = int(args['height'])
-bWidth = int(args['width'])
-board = Board((bHeight,bWidth))
-for _ in board.animate(): ...
+if __name__ == "__main__":
+   ap = argparse.ArgumentParser(add_help = False) # Intilialize Argument Parser
+   ap.add_argument('-h', '--height', default=500)
+   ap.add_argument('-w', '--width', default=500)
+   args = vars(ap.parse_args()) # Gather Arguments
+   height = int(args['height'])
+   width = int(args['width'])
+   board = Board(width,height)
+   for _ in board.animate(): ...
